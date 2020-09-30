510fbdd88546128c8f71c3c89da1cba05b2c3f03
James Hrivnak <jameshrivnak4@gmail.com>
2020-09-30 14:32:11 -0600

    Completed all but one of the tests for stringifyJSON, like getElementsByClassName its not pretty, still have to do [8, [[], 3, 4]] test case. Only am using recursion currently for the [[[['foo']]]] test case

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -2,7 +2,218 @@
 // var stringifyJSON = JSON.stringify;
 
 // but you don't so you're going to write it from scratch:
-
 var stringifyJSON = function(obj) {
-  // your code goes here
+  if (typeof obj === 'number') {
+    return obj.toString();
+  }
+  let resultStr = '';
+  if (obj === null) {
+    return 'null';
+  }
+  if (typeof obj === 'boolean') {
+    return obj.toString();
+  }
+
+  //First Tests for Empty Object- ->
+  // alert(JSON.stringify(obj) + ' ' + typeof obj)
+  for (let key in obj) {
+    if (typeof obj[key] === 'function' || obj[key] === 'undefined') {
+      return '{}';
+    }
+  }
+
+  if (Object.keys(obj).length === 0 && obj.constructor === Object) {
+    return '{}';
+  } else if (obj.constructor === Object) {
+    for (let key in obj) {
+      if (typeof obj[key] === 'boolean' || obj[key] === null) {
+        resultStr = resultStr.concat('"' + key + '":');
+        resultStr = resultStr.concat(obj[key]) + ',';
+      } else {
+        if (obj[key].constructor === Object) {
+          resultStr = resultStr.concat('"' + key + '":');
+          let result = coolerFunc(obj[key]);
+          resultStr = resultStr.concat(result + ',');
+        } else if (Array.isArray(obj[key])) {
+          resultStr = resultStr.concat('"' + key + '":');
+          let result = '';
+          for (let i = 0; i < obj[key].length; i++) {
+            result = result.concat('"' + obj[key][i] + '",');
+          }
+          // if (result[result.length - 1] === ',') {
+          //   result = result.split('');
+          //   result.splice(result.length - 1, 1)
+          //   result = result.join('');
+          // }
+          result = result.split('');
+          if (result[result.length - 1] === ',') {
+            result.splice(result.length - 1, 1);
+          }
+          result.unshift('[');
+          result.push(']');
+          resultStr = resultStr.concat(result.join('') + ',');
+          // resultStr = resultStr.split('');
+          // resultStr.unshift('{');
+          // resultStr.push('}')
+          // return resultStr.join('');
+        } else {
+          resultStr = resultStr.concat('"' + key + '":');
+          resultStr = resultStr.concat('"' + obj[key] + '"');
+          // resultStr = resultStr.concat(',')
+        }
+      }
+    }
+    // if (resultStr[resultStr.length - 1] === ',') {
+    //   resultStr = resultStr.split('')
+    //   resultStr.splice(resultStr.length - 1, 1);
+    //   resultStr = resultStr.join('');
+    // }
+    resultStr = resultStr.split('');
+    if (resultStr[resultStr.length - 1] === ',') {
+      resultStr.splice(resultStr.length - 1, 1);
+    }
+    resultStr.unshift('{');
+    resultStr.push('}');
+    // resultStr = resultStr.join('');
+    // resultStr = resultStr.split(',');
+    resultStr = resultStr.join('');
+    return resultStr;
+  }
+
+  //Array Case- ->
+  if (Array.isArray(obj)) {
+    if (obj.length > 1) {
+      if (obj.every(x => x.constructor === Object)) {
+        for (let i = 0; i < obj.length; i++) {
+          if (obj[i].constructor === Object) {
+            let result = '';
+            for (let key in obj[i]) {
+              result = result.concat('"' + key + '":');
+              result = result.concat('"' + obj[i][key] + '"');
+            }
+            result = result.split('');
+            result.unshift('{');
+            result.push('}');
+            result.push(',');
+            resultStr = resultStr.concat(result.join(''));
+          }
+        }
+        resultStr = resultStr.split('');
+        if (resultStr[resultStr.length - 1] === ',') {
+          resultStr.splice(resultStr.length - 1, 1);
+        }
+        resultStr.unshift('[');
+        resultStr.push(']');
+        return resultStr.join('');
+      }
+      //Every Element in the Array is not an Array
+      if (obj.every(x => !Array.isArray(x))) {
+        for (let key in obj) {
+          if (typeof obj[key] === 'string') {
+            let x = obj[key];
+            x = x.split('');
+            x.splice(0, 0, '"');
+            x.splice(x.length, 0, '"');
+            x = x.join('');
+            resultStr = resultStr.concat(x);
+          } else {
+            resultStr = resultStr.concat(obj[key].toString() + ',');
+          }
+        }
+        if (resultStr[resultStr.length - 1] === ',') {
+          resultStr = resultStr.split('');
+          resultStr.splice(resultStr.length - 1, 1);
+          resultStr = resultStr.join('');
+        }
+
+        resultStr = resultStr.split('');
+        resultStr.unshift('[');
+        resultStr.push(']');
+        return resultStr.join('');
+      }
+
+
+      for (let z = 0; z < obj.length; z++) {
+        if (obj[z].constructor === Object) {
+
+          let result = coolerFunc(obj[z]) + ',';
+          resultStr = resultStr.concat(result);
+        }
+      }
+
+      if (resultStr[resultStr.length - 1] === ',') {
+        resultStr = resultStr.split('');
+        resultStr.splice(resultStr.length - 1, 1);
+        resultStr = resultStr.join('');
+      }
+
+      resultStr = resultStr.split('');
+      resultStr.unshift('[');
+      resultStr.push(']');
+      return resultStr.join('');
+
+
+    } else {
+      if (Array.isArray(obj[0])) {
+        let counter = 0;
+        let result = coolFunc(obj, counter);
+        return result;
+      }
+    }
+    if (typeof obj[0] === 'string') {
+      resultStr = obj[0].split('');
+      resultStr.splice(0, 0, '"');
+      resultStr.splice(resultStr.length, 0, '"');
+      resultStr = resultStr.join('');
+      obj.splice(0, 1, resultStr);
+    }
+    let result = obj;
+    result = result.join('');
+    return '[' + result + ']';
+  }
+
+  //String Case- ->
+  if (typeof obj === 'string') {
+    for (let key in obj) {
+      resultStr = resultStr.concat(obj[key]);
+    }
+  }
+  resultStr = resultStr.split('');
+  resultStr.splice(0, 0, '"');
+  resultStr.splice(resultStr.length, 0, '"');
+  resultStr = resultStr.join('');
+  return resultStr;
 };
+
+let coolFunc = function (obj, counter) {
+  if (!Array.isArray(obj)) {
+    let result = ('"' + obj + '"');
+    result = result.split('');
+    for (let i = 0; i < counter; i++) {
+      result.unshift('[');
+      result.push(']');
+    }
+    return result.join('');
+  }
+  obj.unshift();
+  counter++;
+  return coolFunc(obj[0], counter);
+};
+
+let coolerFunc = function (obj) {
+  let resultStr = '';
+  for (let key in obj) {
+    resultStr = resultStr.concat('"' + key + '":"' + obj[key] + '"');
+  }
+  resultStr = resultStr.split('');
+  resultStr.unshift('{');
+  resultStr.push('}');
+  return resultStr.join('');
+};
+
+let handleArray = function (array) {
+  if (array.length === 0) {
+    return '[]';
+  }
+};
+
